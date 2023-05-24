499e76c54334618adb72d8dae0e373c1c1a1c93d
Wh-dseatroops <798208052@qq.com>
2023-05-24 12:33:36 +0800

    第一次使用IDEA提交到本地仓库

---
+++ TestModul01/src/com/dhb/Student.java
@@ -0,0 +1,16 @@
+package com.dhb;
+
+/**
+ * @Auther:代海兵 设置@Auther File and Code Templates  设置快捷代码 Live templates
+ * @Date: -2023 - 05 - 24 - 12:19
+ * @Description:com.dhb
+ * @version:1.0
+ * @shortcut_key1: ctrl+D复制一行 ctrl+Y删除一行  alt+insert生成代码 alt+Enter导包+生成变量等
+ * @shortcut_key2: alt+7 显示代码结构 alt+1 显示导航栏  shift+f6重合名 ctrl+n搜索类 ctrl+alt+L格式化
+ * @shortcut_key3: main sout fori
+ * @shortcut_key4: ctrl+/单行注释或ctrl+shift+/文档注释  shift+alt选中光标  ctrl+shift+向下或向上移动一行代码
+ */
+
+public class Student {
+
+}
